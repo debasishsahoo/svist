45bb2ffa25156de5abac7d930902bca7e9cb1a4c
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-07-16 14:47:54 +0530

    Tenth Commit

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,23 +1,6 @@
 const router= require('express').Router();
 const userController = require('../controllers/user.controller');
 
-router.get('/', userController.allUsers);
-router.get('/:id', userController.singleUser);
+router.get('/:id', userController.getUserById);
 
-
-
-
-
-
-
-router.post('/', (req, res) => {
-  res.send('User route-POST with body: ' + JSON.stringify(req.body));
-});
-router.put('/:id', (req, res) => {
-  res.send('User route-PUT for ID: ' + req.params.id +
-     ' with body: ' + JSON.stringify(req.body));
-});
-router.delete('/:id', (req, res) => {
-  res.send('User route-DELETE for ID: ' + req.params.id);
-});
 module.exports = router;
