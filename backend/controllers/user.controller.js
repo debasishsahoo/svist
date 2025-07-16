b756b55b38db912da85a97a351552bf0599b12de
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-07-16 16:20:44 +0530

    12 Commit

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -21,10 +21,20 @@ const userController = {
           404
         ));
       }
-      res.status(200).json({ message: "User found", data: user });
+      res.status(200).json(generateResponse(
+          true,
+          "User found",
+          user,
+          200
+        ));
     } catch (error) {
       console.error("Error fetching user:", error);
-      res.status(500).json({ message: "Failed to fetch user", data: null });
+      res.status(500).json(generateResponse(
+          false,
+          "Failed to Fetch User",
+          null,
+          500
+        ));
     }
   },
   createUsers: async (req, res) => {},
