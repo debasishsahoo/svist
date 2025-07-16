561540a0d1ddcd02427bf9c210996e2f2f7277f4
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-07-16 12:56:09 +0530

    Seventh Commit

--- backend/utils/fileManager.js
+++ backend/utils/fileManager.js
@@ -7,16 +7,40 @@ class FileManager{
     this.dataDir = dataDir;
   }
  async readData(filename){
-    try{}
-    catch(error){}
+    try{
+      const filePath = path.join(this.dataDir, filename);
+      const data = await fs.readFile(filePath, "utf8");
+      return JSON.parse(data);
+    }
+    catch(error){
+        if (error.code === "ENOENT") {
+        return [];
+      }
+      throw error;
+    }
  }
  async writeData(filename,data){
-    try{}
-    catch(error){}
+     try {
+      const filePath = path.join(this.dataDir, filename);
+      await fs.writeFile(filePath, JSON.stringify(data, null, 2));
+      return true;
+    } catch (error) {
+      throw error;
+    }
  }
- async updateData(filename,data){
-    try{}
-    catch(error){}
+ async updateData(filename,updateData){
+   const data = await this.readData(filename);
+    const index = data.findIndex((item) => item.id === parseInt(id));
+    if (index === -1) {
+      return null;
+    }
+    data[index] = {
+      ...data[index],
+      ...updateData,
+      updatedAt: new Date().toISOString(),
+    };
+    await this.writeData(filename, data);
+    return data[index];
  }
  async deleteData(filename,id){
     try{}
