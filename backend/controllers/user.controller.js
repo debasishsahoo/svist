bb499417fe7a29c75dae461f094037ba07b12cd9
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-07-16 15:15:41 +0530

    Tenth Commit

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -3,8 +3,12 @@ const fileManager = require("../utils/fileManager");
 const userController = {
   getUserById: async (req, res) => {
     try {
-  const {id}=req.param;
-  const users=await fileManager.readFile("users.json");
+  const {id}=req.params;
+  console.log('id:', id)
+
+  const users=await fileManager.readData("users.json");
+  console.log('users:', users)
+  
   const user = users.find(user => user.id === parseInt(id));
   if(!user){
     return res.status(404).json({ message: "User not found",data:null });
@@ -12,6 +16,7 @@ const userController = {
   res.status(200).json({message: "User found",data: user });
 
     } catch (error) {
+      console.error("Error fetching user:", error);
      res.status(500).json({message: "Failed to fetch user",data: null }); 
     }
   },
