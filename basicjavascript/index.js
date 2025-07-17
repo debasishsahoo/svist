844f6d0e4965dbd2093707083531ab44abdc34bc
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-07-17 15:19:40 +0530

    28th commit

--- basicjavascript/index.js
+++ basicjavascript/index.js
@@ -52,45 +52,73 @@
 // fetchData();
 
 
-try {
-  const result = 10 / 0;
-  console.log(result); // No error (Infinity)
+// try {
+//   const result = 10 / 0;
+//   console.log(result); // No error (Infinity)
 
-  const invalidFunc = nonExistentFunction(); // Throws ReferenceError
+//   const invalidFunc = nonExistentFunction(); // Throws ReferenceError
 
 
 
-} catch (error) {
-  console.error("Error caught:", error.message); // "nonExistentFunction is not defined"
-} finally {
-  console.log("This always runs.");
+// } catch (error) {
+//   console.error("Error caught:", error.message); // "nonExistentFunction is not defined"
+// } finally {
+//   console.log("This always runs.");
+// }
+
+
+// function divide(a, b) {
+//   try {
+//     if (b === 0) throw new Error("Division by zero!");
+//     return a / b;
+//   } catch (error) {
+//     return error.message;
+//   }
+// }
+
+// console.log(divide(10, 2)); //Output-5
+// console.log(divide(10, 0)); //
+
+// try {
+//   try {
+//     nonExistentFunction(); // Throws ReferenceError
+//   } catch (innerError) {
+//     console.error("Inner catch:", innerError.message);
+//     throw new Error("Rethrown error!"); // Re-throw to outer catch
+//   }
+// } catch (outerError) {
+//   console.error("Outer catch:", outerError.message);
+// }
+
+const fruits = ["A", "B", "C"];
+
+fruits.forEach((fruit, index) => {
+  console.log(`${index}: ${fruit}`);
+});
+// Output:
+// 0: A
+// 1: B
+// 2: C
+
+
+
+const numbers = [1, 2, 3, 4, 5];
+const evens = numbers.filter(num => num % 2 === 0);
+
+console.log(evens);
+
+const person = { name: "Alice", age: 25 };
+
+for (const key in person) {
+  console.log(`${key}: ${person[key]}`);
+}
+const fruits2 = ["A", "B", "C"];
+
+for (const fruit of fruits2) {
+  console.log(fruit);
 }
 
 
-function divide(a, b) {
-  try {
-    if (b === 0) throw new Error("Division by zero!");
-    return a / b;
-  } catch (error) {
-    return error.message;
-  }
-}
 
-console.log(divide(10, 2)); //Output-5
-console.log(divide(10, 0)); //
-
-try {
-  try {
-    nonExistentFunction(); // Throws ReferenceError
-  } catch (innerError) {
-    console.error("Inner catch:", innerError.message);
-    throw new Error("Rethrown error!"); // Re-throw to outer catch
-  }
-} catch (outerError) {
-  console.error("Outer catch:", outerError.message);
-}
-
-
-
-
-
+const [a, b, c] = [1, 2, 3];
+console.log(a, b, c);
