c1136088f7e006f7da88e5d11f8bcdcf8e66a634
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-07-17 12:21:46 +0530

    23th commit

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,15 @@
 const router= require('express').Router();
 const userController = require('../controllers/user.controller');
 
+router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getUserById);
+
+router.post('/',userController.createUsers);
+router.put('/:id',userController.updateUsers);
+router.delete('/:id',userController.deleteUsers);
+
+
+
+
 
 module.exports = router;
