fe6b3a7409532eadd1d5497b05f75888c57f5f12
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-07-17 15:05:50 +0530

    27th commit

--- basicjavascript/index.js
+++ basicjavascript/index.js
@@ -52,11 +52,43 @@
 // fetchData();
 
 
+try {
+  const result = 10 / 0;
+  console.log(result); // No error (Infinity)
+
+  const invalidFunc = nonExistentFunction(); // Throws ReferenceError
 
 
 
+} catch (error) {
+  console.error("Error caught:", error.message); // "nonExistentFunction is not defined"
+} finally {
+  console.log("This always runs.");
+}
 
 
+function divide(a, b) {
+  try {
+    if (b === 0) throw new Error("Division by zero!");
+    return a / b;
+  } catch (error) {
+    return error.message;
+  }
+}
+
+console.log(divide(10, 2)); //Output-5
+console.log(divide(10, 0)); //
+
+try {
+  try {
+    nonExistentFunction(); // Throws ReferenceError
+  } catch (innerError) {
+    console.error("Inner catch:", innerError.message);
+    throw new Error("Rethrown error!"); // Re-throw to outer catch
+  }
+} catch (outerError) {
+  console.error("Outer catch:", outerError.message);
+}
 
 
 
