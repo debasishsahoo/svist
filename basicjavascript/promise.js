4a0f936bf57ecd927a02f19223017f9fe345bf73
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-07-17 14:47:22 +0530

    26th commit

--- basicjavascript/promise.js
+++ basicjavascript/promise.js
@@ -15,3 +15,22 @@ function traditionalPromise() {
   console.log("This runs immediately (non-blocking)\n");
 }
 traditionalPromise()
+
+
+async function asyncAwaitExample() {
+  console.log("2. Async/Await Example:");
+
+  // Creating the same promise
+  const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Async/Await result!");
+    }, 2000);
+  });
+
+  // Using await to wait for the promise
+  const result = await myPromise;
+  console.log("Result:", result);
+  console.log("This runs after the promise resolves\n");
+}
+
+asyncAwaitExample()
