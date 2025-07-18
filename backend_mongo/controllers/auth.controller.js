b80e6e6b6225395588de2a948bc6241361b0d294
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-07-18 16:51:05 +0530

    4th Commmit

--- backend_mongo/controllers/auth.controller.js
+++ backend_mongo/controllers/auth.controller.js
@@ -1,7 +1,46 @@
 const User = require("../models/user.model");
 const winston = require("winston");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
-exports.register=async (req,res)=>{}
-exports.login=async (req,res) => {}
-exports.getCurrentUser=async (req,res)=>{}
+exports.register = async (req, res) => {
+  try {
+    const { email, password, name } = req.body;
+    let user = await User.findOne({ email });
+    if (user) return res.status(400).send("User already registered");
+
+    user = new User({ name, email, password });
+    await user.save();
+
+    const token = user.generateAuthToken();
+    res.header("x-auth-token", token).send({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } catch (err) {
+    winston.error(err.message, err);
+    res.status(500).send("Something failed");
+  }
+};
+exports.login = async (req, res) => {
+    try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) return res.status(400).send("Invalid email or password");
+
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword)
+      return res.status(400).send("Invalid email or password");
+
+    const token = user.generateAuthToken();
+    res.send(token);
+  } catch (err) {
+    winston.error(err.message, err);
+    res.status(500).send("Something failed");
+  }
+};
+exports.getCurrentUser = async (req, res) => {
+    const user = await User.findById(req.user._id).select("-password");
+  res.send(user);
+};
