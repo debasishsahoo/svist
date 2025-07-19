ff703359546602af084b4f259988b8312b88dfc8
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-07-19 11:51:43 +0530

    Last Commit

--- backend_mongo/routes/user.route.js
+++ backend_mongo/routes/user.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth.controller');
+const auth = require('../middlewares/auth.middleware');
 
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
-router.get('/me', authController.getCurrentUser);
+router.get('/me',auth, authController.getCurrentUser);
 
 module.exports = router;
