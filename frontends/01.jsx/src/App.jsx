5a01fb1024c5105d69d3c017d785c78da5a2eaab
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-07-21 16:00:40 +0530

    04

--- frontends/01.jsx/src/App.jsx
+++ frontends/01.jsx/src/App.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Intro from "./components/Intro";
 import AdvancedIntro01 from "./components/AdvancedIntro01";
-import AdvancedIntro02 from "./components/AdvancedIntro02"; // Assuming you have a CSS file for styling
+import AdvancedIntro02 from "./components/AdvancedIntro02";
+import AdvancedIntro03 from "./components/AdvancedIntro03";
+import AdvancedIntro04 from "./components/AdvancedIntro04"; // Assuming you have a CSS file for styling
 
 function App() {
   return (
     <div>
       <h1>Welcome to JSX </h1>
-      <AdvancedIntro02/>
+      <AdvancedIntro04 />
+      <br />
+      {/* <AdvancedIntro03 />
+      <br /> */}
+      {/* <AdvancedIntro02/> */}
       {/* <AdvancedIntro01 /> */}
       {/* <Intro/> */}
     </div>
---
+++ frontends/01.jsx/src/components/AdvancedIntro04.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "../styles/s2.css";
+//internal css
+const styleObj = {
+  backgroundColor: "lightblue",
+  padding: "20px",
+  borderRadius: "5px",
+  fontSize: "16px",
+};
+const AdvancedIntro04 = () => {
+  // Inline styles using objects
+  const styledElement = <div style={styleObj}>Styled content</div>;
+
+  const isActive = false;
+  const conditionalStyling = (
+    <button
+    //external css
+      className={`btn ${isActive ? "btn-active" : "btn-inactive"}`}
+      // Inline styles based on condition
+      // This will apply a different opacity based on the isActive state
+      style={{
+        opacity: isActive ? 1 : 0.5,
+      }}
+    >
+      {isActive ? "Active" : "Inactive"}
+    </button>
+  );
+
+  return (
+    <>
+      <div>{styledElement}</div>;
+      <br />
+      {conditionalStyling}
+    </>
+  );
+};
+
+export default AdvancedIntro04;
