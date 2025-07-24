dfec1b9c9f11e1ce2f42a3de034d4b83d9e3f1fa
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-07-24 12:25:41 +0530

    New Update3

---
+++ taskmanagementapp/backend/routes/tasks.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+
+router.get('/');
+router.get('/:id');
+router.post('/');
+router.put('/:id');
+router.put('/:id/toggle');
+router.delete("/:id");
+router.get('stats');
+module.exports = router;
