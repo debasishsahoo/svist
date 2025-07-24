e166cc135f3a95c5c7fe2309e64a7169828b602e
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-07-24 15:40:25 +0530

    New Update20

--- taskmanagementapp/backend/controllers/taskController.js
+++ taskmanagementapp/backend/controllers/taskController.js
@@ -1,11 +1,9 @@
 const Task = require("../models/Task");
 
-exports.getAllTask= async (req, res) => {
-    
-}
-exports.getSingleTask= async (req, res) => {}
-exports.createTask= async (req, res) => {
-     try {
+exports.getAllTask = async (req, res) => {};
+exports.getSingleTask = async (req, res) => {};
+exports.createTask = async (req, res) => {
+  try {
     const taskData = {
       ...req.body,
       user: req.user._id,
@@ -19,8 +17,30 @@ exports.createTask= async (req, res) => {
     console.error("Create task error:", error);
     res.status(500).json({ message: "Server error while creating task" });
   }
-}
-exports.updateTask= async (req, res) => {}
-exports.toggleTask= async (req, res) => {}
-exports.deleteTask= async (req, res) => {}
-exports.stats= async (req, res) => {}
+};
+exports.updateTask = async (req, res) => {
+  try {
+    const task = await Task.findOne({
+      _id: req.params.id,
+      user: req.user._id,
+    });
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    // Update task fields
+    Object.keys(req.body).forEach((key) => {
+      task[key] = req.body[key];
+    });
+
+    await task.save();
+    res.json(task);
+  } catch (error) {
+    console.error("Update task error:", error);
+    res.status(500).json({ message: "Server error while updating task" });
+  }
+};
+exports.toggleTask = async (req, res) => {};
+exports.deleteTask = async (req, res) => {};
+exports.stats = async (req, res) => {};
