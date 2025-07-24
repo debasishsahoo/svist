a992df3cd9566a2e8369bf7a8ff9079f21e11cb2
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-07-24 12:50:51 +0530

    New Update7

--- taskmanagementapp/backend/controllers/authController.js
+++ taskmanagementapp/backend/controllers/authController.js
@@ -3,3 +3,7 @@ const User = require("../models/User");
 exports.register= async (req, res) => {}
 exports.login= async (req, res) => {}
 exports.logout= async (req, res) => {}
+exports.getProfile= async (req, res) => {}
+exports.updateProfile= async (req, res) => {}
+exports.updatePassword= async (req, res) => {}
+exports.deleteAccount= async (req, res) => {}
