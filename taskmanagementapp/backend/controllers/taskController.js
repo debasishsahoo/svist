4b80da48350a850e5a571b52aaf164b2cee6aacd
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-07-24 15:38:35 +0530

    New Update19

--- taskmanagementapp/backend/controllers/taskController.js
+++ taskmanagementapp/backend/controllers/taskController.js
@@ -1,8 +1,25 @@
 const Task = require("../models/Task");
 
-exports.getAllTask= async (req, res) => {}
+exports.getAllTask= async (req, res) => {
+    
+}
 exports.getSingleTask= async (req, res) => {}
-exports.createTask= async (req, res) => {}
+exports.createTask= async (req, res) => {
+     try {
+    const taskData = {
+      ...req.body,
+      user: req.user._id,
+    };
+
+    const task = new Task(taskData);
+    await task.save();
+
+    res.status(201).json(task);
+  } catch (error) {
+    console.error("Create task error:", error);
+    res.status(500).json({ message: "Server error while creating task" });
+  }
+}
 exports.updateTask= async (req, res) => {}
 exports.toggleTask= async (req, res) => {}
 exports.deleteTask= async (req, res) => {}
