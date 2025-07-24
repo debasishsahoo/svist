80ea4011bc471268743f5267077b1f2f1d500c8b
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-07-24 13:33:37 +0530

    New Update12

--- taskmanagementapp/backend/controllers/authController.js
+++ taskmanagementapp/backend/controllers/authController.js
@@ -1,11 +1,46 @@
 const User = require("../models/User");
-const { body } = require('express-validator');
-const { handleValidationErrors } = require('../middleware/validation');
+const { body } = require("express-validator");
+const { handleValidationErrors } = require("../middleware/validation");
 
-exports.register= async (req, res) => {}
-exports.login= async (req, res) => {}
-exports.logout= async (req, res) => {}
-exports.getProfile= async (req, res) => {}
-exports.updateProfile= async (req, res) => {}
-exports.updatePassword= async (req, res) => {}
-exports.deleteAccount= async (req, res) => {}
+exports.register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "User already exists with this email" });
+    }
+  } catch (error) {}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+exports.login = async (req, res) => {};
+exports.logout = async (req, res) => {};
+exports.getProfile = async (req, res) => {};
+exports.updateProfile = async (req, res) => {};
+exports.updatePassword = async (req, res) => {};
+exports.deleteAccount = async (req, res) => {};
