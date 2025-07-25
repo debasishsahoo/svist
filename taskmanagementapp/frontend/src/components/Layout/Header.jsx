918af1b757fa0e22600bf8119d0ed5e8affca5f9
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-07-25 13:50:23 +0530

    Fornt End11

--- taskmanagementapp/frontend/src/components/Layout/Header.jsx
+++ taskmanagementapp/frontend/src/components/Layout/Header.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
+import "./Layout.css";
 
 const Header = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
 
-export default Header
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
+  return (
+    <header>
+      <div>
+        <Link to="/" className="logo">
+          <h1>Task Manager</h1>
+        </Link>
+        <nav className="nav">
+            {user?():()}
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
